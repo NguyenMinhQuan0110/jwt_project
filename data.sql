bb5d66cdcd4e91d24425e866be46b21047153ff7
NguyenMinhQuan0110 <minhquan11003@gmail.com>
2025-06-23 16:58:05 +0700

    update data

--- data.sql
+++ data.sql
@@ -13,13 +13,26 @@ CREATE TABLE `user` (
    `block` bit(1) DEFAULT NULL,
    `reason` varchar(255) DEFAULT NULL,
    `loginfail` int DEFAULT NULL,
+   `block_expiry` datetime(6) DEFAULT NULL,
+   `created_by` varchar(255) DEFAULT NULL,
+   `created_time` datetime(6) DEFAULT NULL,
+   `update_by` varchar(255) DEFAULT NULL,
+   `update_time` datetime(6) DEFAULT NULL,
    PRIMARY KEY (`id`),
    KEY `FKn82ha3ccdebhokx3a8fgdqeyy` (`role_id`),
    CONSTRAINT `FKn82ha3ccdebhokx3a8fgdqeyy` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
- ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `role` (
+ ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ 
+ CREATE TABLE `role` (
    `id` bigint NOT NULL AUTO_INCREMENT,
    `name` varchar(255) DEFAULT NULL,
    PRIMARY KEY (`id`)
- ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-  
+ ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ 
+ CREATE TABLE `history` (
+   `id` bigint NOT NULL AUTO_INCREMENT,
+   `action` varchar(255) DEFAULT NULL,
+   `dateimp` datetime(6) DEFAULT NULL,
+   `implementer` varchar(255) DEFAULT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
